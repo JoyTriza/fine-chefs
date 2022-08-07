fc5e1720b8aa9dd82c3eb9ca58e83581a5ebd629
Trizah <wangarijoytriza@gmail.com>
2022-08-07 21:15:08 +0300

    Add logo

---
+++ client/src/Components/Home/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navbar';
+
+function Landing() {
+  return (
+    <div className='land h-screen w-screen bg-cover bg-no-repeat'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Landing;
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Navbar() {
+  return (
+    <div className='logo'>
+        <div className='navbar-heading px-3 py-4'>
+            <h1>Fine <span className='text-2xl text-yellow-500	'>Chef's</span></h1>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
