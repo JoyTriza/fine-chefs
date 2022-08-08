3cc2aed90d6a77965df1ac20f13352a3eaa098f5
Trizah <wangarijoytriza@gmail.com>
2022-08-08 11:27:03 +0300

    Validation

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function Signup() {
+const [error, setError] = useState([]);
 const[person, setPerson] = useState({
   last_name:"", 
   first_name:"",
@@ -18,21 +19,46 @@ const[person, setPerson] = useState({
     });
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/users",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(person)
+    })
+      .then(res => {
+      if(res.ok){
+        res.json().then(person => console.log(person))
+      } else {
+        res.json().then(e => setError(Object.entries(e.error).flat()))
+      }
+    })
+  }
+
 
   return (
     <div>
-        <form className='h-screen flex justify-center items-center'>
+        <form onSubmit={handleSubmit} className='h-screen flex justify-center items-center'>
             <div className='flex flex-col justify-around h-2/5 w-1/2'>
             <input onChange={handleChange} value= {person.first_name} name='first_name' type='first-name' placeholder='First-name...' className='placeholder:text-xs placeholder:text-black pl-1 outline-none border-black border-2 bg-transparent'/>   
             <input onChange={handleChange} value= {person.last_name} name='last_name' type='last-name' placeholder='Last-name...' className='placeholder:text-xs placeholder:text-black pl-1 outline-none border-black border-2 bg-transparent'/>         
             <input onChange={handleChange} value= {person.email} name='email'type='email' placeholder='Email...' className='placeholder:text-xs placeholder:text-black pl-1 outline-none border-black border-2 bg-transparent'/>      
             <input onChange={handleChange} value= {person.password} name='password' type='password' placeholder='Password...' className='placeholder:text-xs placeholder:text-black pl-1 outline-none border-black border-2 bg-transparent'/>
-            <input onChange={handleChange} value= {person.password_confirmation} name='password_confirmation'type='Confirm password' placeholder='Confirm Password...' className='placeholder:text-xs placeholder:text-black pl-1 outline-none border-black border-2 bg-transparent'/>
+            <input onChange={handleChange} value= {person.password_confirmation} name='password_confirmation'type='password' placeholder='Confirm Password...' className='placeholder:text-xs placeholder:text-black pl-1 outline-none border-black border-2 bg-transparent'/>
             <div className='flex justify-end'>
                 <div className='flex flex-col items-end'>
-                  <button className='border-solid border-2 border-black px-2 hover:bg-red-500 w-3/4'>Signup</button>
+                  <button type='submit' className='border-solid border-2 border-black px-2 hover:bg-red-500 w-3/4'>Signup</button>
                   <Link to='../login' className='hover:text-red-500'>Already have an account?</Link>
                 </div>
+                {error?.length > 0 && (
+                <ul style={{ color: "red" }}>
+                  {error.map((error) => (
+                  <li className='ml-2 text-red-400' key={error}>{error}</li>
+                  ))}
+              </ul>
+            )}
             </div>
             </div>
         </form>
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :show, :destroy]
+  resources :users, only: [:create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/hello', to: 'application#hello_world'
 
   # Defines the root path route ("/")
   # root "articles#index"
